33efbd5287d38265b145b422b0b1899ecab79607
Cynthia Teeters <glean@cynthiateeters.com>
2020-06-11 02:10:35 -0400

    add origami brd as svg to index page

--- src/content/pages/index.md
+++ src/content/pages/index.md
@@ -3,12 +3,11 @@ template: index-page
 slug: /
 title: IDMX
 tagline: Interface Design & Web Development
-featuredImage: /assets/frame-2x.png
 cta:
   ctaText: Know more
   ctaLink: /about
 ---
-**What is IDMX?**
 
+**What is IDMX?**
 
 Interfaces are the lens through which we view our modern digital lives. Every interface, every point of contact with a computer system, must be designed by someone who has a deep understanding of how humans and computers mesh. Our favorite websites and apps on our computers and mobile devices, the screens in our vehicles, interactive displays in museums, and touchscreen kiosks - all of these things are connected via the Web and driven by interfaces.
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,14 +2,15 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import { RiArrowRightSLine } from "react-icons/ri"
+import Icon from "../assets/images/icon.svg"
 
 import Layout from "../components/layout"
 import BlogListHome from "../components/blog-list-home"
 import SEO from "../components/seo"
 
 export const pageQuery = graphql`
-  query HomeQuery($id: String!){
-		markdownRemark(id: { eq: $id }) {
+  query HomeQuery($id: String!) {
+    markdownRemark(id: { eq: $id }) {
       id
       html
       frontmatter {
@@ -17,7 +18,12 @@ export const pageQuery = graphql`
         tagline
         featuredImage {
           childImageSharp {
-            fluid(maxWidth: 480, maxHeight: 380, quality: 80, srcSetBreakpoints: [960, 1440]) {
+            fluid(
+              maxWidth: 480
+              maxHeight: 380
+              quality: 80
+              srcSetBreakpoints: [960, 1440]
+            ) {
               ...GatsbyImageSharpFluid
             }
             sizes {
@@ -37,30 +43,45 @@ export const pageQuery = graphql`
 const HomePage = ({ data }) => {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
-  const Image = frontmatter.featuredImage ? frontmatter.featuredImage.childImageSharp.fluid : ""
-	return (
-		<Layout>
-      <SEO/>
+  const Image = frontmatter.featuredImage
+    ? frontmatter.featuredImage.childImageSharp.fluid
+    : ""
+  return (
+    <Layout>
+      <SEO />
       <div className="home-banner grids col-1 sm-2">
         <div>
           <h1 class="title">{frontmatter.title}</h1>
           <p class="tagline">{frontmatter.tagline}</p>
-          <div className="description" dangerouslySetInnerHTML={{__html: html}}/>
-          <Link to={frontmatter.cta.ctaLink} className="button">{frontmatter.cta.ctaText}<span class="icon -right"><RiArrowRightSLine/></span></Link>
+          <div
+            className="description"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+          <Link to={frontmatter.cta.ctaLink} className="button">
+            {frontmatter.cta.ctaText}
+            <span class="icon -right">
+              <RiArrowRightSLine />
+            </span>
+          </Link>
         </div>
         <div>
           {Image ? (
-            <Img 
-              fluid={Image} 
-              alt={frontmatter.title + ' - Featured image'}
+            <Img
+              fluid={Image}
+              alt={frontmatter.title + " - Featured image"}
               className="featured-image"
             />
-          ) : ""}
+          ) : (
+            <Icon
+              alt={frontmatter.title + " - Featured image"}
+              className="featured-image"
+            />
+          )}
         </div>
       </div>
-      <BlogListHome/>
-		</Layout>
-	)
+      <BlogListHome />
+    </Layout>
+  )
 }
 
 export default HomePage
