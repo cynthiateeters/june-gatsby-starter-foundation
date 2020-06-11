8a277e30c6ef82210c8e22692d0850bfbd5e9075
Cynthia Teeters <glean@cynthiateeters.com>
2020-06-11 00:54:12 -0400

    Fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
-import { RiHeart2Line } from "react-icons/ri"
+import { useStaticQuery, graphql } from "gatsby"
+/* import { Link } from "gatsby" */
+import { RiOpenArmLine, RiCopyrightLine } from "react-icons/ri"
 
-const Footer = () => (
-  <footer className="site-footer">
-    <div className="container">
-      <p>&copy; 2020</p>
-    </div>
-  </footer>
-)
+const Footer = () => {
+  const query = graphql`
+    query FooterQuery {
+      site {
+        siteMetadata {
+          siteTitle: title
+          siteAuthor: author
+        }
+      }
+    }
+  `
+
+  const { site } = useStaticQuery(query)
+  const { siteTitle, siteAuthor } = site.siteMetadata
+
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <p>
+          {siteTitle} <RiOpenArmLine /> Copyright <RiCopyrightLine />{" "}
+          {siteAuthor} 2020
+        </p>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
